672f4947aea088ea5e390e5f3f6f08cf78429f94
sharonf <sharon.fraiman@exaware.com>
2023-03-11 01:26:33 +0200

    Move files to DUT

--- dut_ctrl/setup_dut.py
+++ dut_ctrl/setup_dut.py
@@ -9,12 +9,41 @@ logging.basicConfig(
                     level=logging.INFO,
                     datefmt='%H:%M:%S')
 
+# ***************************************************************************************
+# Module Helper functions
+# ***************************************************************************************
+def _remote_exists(sftp, path):
+    """ 
+    Check if a remote path exists
+    """
+    try:
+        sftp.stat(path)
+        return True
+    except IOError:
+        return False
 
+def _run_remote_shell_cmd(ssh_object, cmd_string) :
+    """
+    Run remote sheel command
+    """
+    import socket
+    try :
+        # Execute a command on the remote server and get the output
+        stdin, stdout, stderr = ssh_object.exec_command(cmd_string)
+    except paramiko.SSHException as e:
+        # Handle SSH exception
+        logging.error(f'SSH error: {e}')
+    except socket.error as e:
+        # Handle socket error
+        logging.error(f'Network error: {e}')
 
+# ***************************************************************************************
+# Main function
+# ***************************************************************************************
 def create_directory_and_copy_files (host, workdir, copy_file_list):
     """
     Move testing files into directory in DUT.
-    If already exists, first delete directory.
+    If already exists, first delete workdir directory.
     """
     # create an SSH client
     with paramiko.SSHClient() as ssh:
@@ -22,20 +51,12 @@ def create_directory_and_copy_files (host, workdir, copy_file_list):
         logging.info(f"Connecting to host {host}")
         ssh.connect(hostname=host, username="root", password="root")
 
-        # create an SFTP client
+        # Create an SFTP client
         with ssh.open_sftp() as sftp:
-
-            # define a function to check if a remote path exists
-            def rexists(sftp, path):
-                try:
-                    sftp.stat(path)
-                    return True
-                except IOError:
-                    return False
-
-            if rexists(sftp, workdir) :
-                logging.warning(f"Removing working directory {workdir}")
-                sftp.remove(workdir)
+            
+            if _remote_exists(sftp, workdir) :
+                logging.info(f"Removing working directory {workdir}")
+                _run_remote_shell_cmd(ssh, f'rm -rf {workdir}')
 
             logging.info(f"create a new folder for workspace {workdir}")           
             sftp.mkdir(workdir)
@@ -45,8 +66,10 @@ def create_directory_and_copy_files (host, workdir, copy_file_list):
                 sftp.put(file, workdir + "/" + file)
 
 if __name__ == "__main__" :
+    # Test constants
     HOST = '10.3.10.10'
     WORKDIR = "/root/workspace"
     COPY_FILE_LIST = ["server.py", "monitor_logfile.py"]
 
+    # Run operation
     create_directory_and_copy_files(HOST, WORKDIR, COPY_FILE_LIST)
