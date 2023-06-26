5b12ec3706f5e99027f42e8473648aa691c0cac5
sharonf <sharon.fraiman@exaware.com>
2023-06-27 02:11:08 +0300

    WIP - Adding support for QOS

--- dut_ctrl/packet_creator.py
+++ dut_ctrl/packet_creator.py
@@ -34,3 +34,29 @@ def create_icmp_frame(src_ip, dst_ip, dst_mac) :
     """
     frame = Ether(dst = dst_mac) / IP(src = src_ip, dst = dst_ip) / ICMP()
     return bytes(frame).hex()
+
+def create_l3_dscp_frame(src_ip: str, dst_ip: str, dst_mac: str , tos->int) -> bytes :
+    """
+    Create a frame
+    Return value : String representation of the hex frame value
+    For example :
+        >>> frame =  Ether(dst='90:3c:b3:04:60:31') / IP(src='54.46.1.2', dst = '54.47.1.2', tos = 0x88)
+        >>> frame_bytes = bytes(frame).hex()
+        >>> frame_bytes
+        '903cb304603100155de27a690800458800140001000040000c01362e0102362f0102'
+        >>> Ether(bytes.fromhex(frame_bytes))
+        <Ether  dst=90:3c:b3:04:60:31 src=00:15:5d:e2:7a:69 type=IPv4 |
+             <IP  version=4 ihl=5 tos=0x88 len=20 id=1 flags= frag=0 ttl=64 proto=hopopt chksum=0xc01 
+                  src=54.46.1.2 dst=54.47.1.2 |>>
+    """
+
+    # x-eth 0/0/46 MAC value : 90:3c:b3:04:60:31
+    #              IP        : 54.46.1.1/24
+
+    # x-eth 0/0/47 IP        : 54.47.1.1/24
+    # a = IP(dst="172.31.0.1", tos=184) # 184 is the decimal equivalent of 0xB8 which is the hex 
+    #                                    af41 is TOS 0x88 goes to queue af3
+    # TX from BCMRM : tx 10 PSRC=47 DATA=903cb304603100155de27a690800458800140001000040000c01362e0102362f0102
+    
+    frame = Ether(dst = dst_mac) / IP(src = src_ip, dst = dst_ip, tos = tos)
+    return bytes(frame).hex()
