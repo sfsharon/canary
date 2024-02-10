69a6ab7721490bd582010d6bddd99541b32d922d
sharonf <sharon.fraiman@exaware.com>
2024-02-10 09:52:48 +0200

    FSM VPN - Working basic features

--- utility/vpn_con_FSM.py
+++ utility/vpn_con_FSM.py
@@ -15,46 +15,53 @@ logging.basicConfig(
 # Action functions for the FSM
 # ====================================================
 def ErrorFSM (fsm):
-    logging.error(f"** ErrorFSM **: Current state:= - {fsm.current_state}")
-    logging.error(str(fsm.input_symbol))
+    logging.error(f"** ErrorFSM ** ({fsm.current_state}). Input Symbol : {fsm.input_symbol}, Connection Established : {fsm.memory['connection_established']}")
     sys.exit(1)
 
 def KillPexpectProc(fsm) :
-    logging.info(f'** KillPexpectProc ** : Current state - {fsm.current_state}')
-    fsm.memory['reset_connection'] = True
+    logging.info(f'** KillPexpectProc ** ({fsm.current_state})')
+    fsm.memory['reset_connection']    = True
 
 def EnterPassword (fsm):
-    logging.info(f'** EnterPassword ** : Current state - {fsm.current_state}')
+    logging.info(f'** EnterPassword ** ({fsm.current_state})')
     fsm.memory['response_to_pexpect'] = '123456'
 
+def ConnEstablished (fsm) :
+    logging.info(f'** ConnEstablished ** ({fsm.current_state})')
+    fsm.memory['connection_established'] = True
 
 # FSM Constants
 # ====================================================
-TWO_FACTOR_REQUEST_SYMBOL = "Two-factor authentication token:"
-PASSWORD_REQUEST_SYMBOL   = "\[sudo\] password for sharonf:"
-CLOSED_CONNECTION_SYMBOL  = "Closed connection to gateway"
+TWO_FACTOR_REQUEST_SYMBOL    = "Two-factor authentication token:"
+PASSWORD_REQUEST_SYMBOL      = "\[sudo\] password for sharonf:"
+CLOSED_CONNECTION_SYMBOL     = "Closed connection to gateway"
+SUCCESSFUL_CONNECTION_SYMBOL = "Tunnel is up and running"
 EXPECTED_SYMBOLS = [PASSWORD_REQUEST_SYMBOL,
                     TWO_FACTOR_REQUEST_SYMBOL, 
                     CLOSED_CONNECTION_SYMBOL,
+                    SUCCESSFUL_CONNECTION_SYMBOL,
                     # last two elements are always EOF and TIMEOUT
                     pexpect.EOF, 
                     pexpect.TIMEOUT]
 CMD = 'sudo /usr/bin/openfortivpn -c /home/sharonf/my.cfg'
-EXPECT_TIMEOUT = 3
+EXPECT_TIMEOUT = 10
 
 def main() :
 
-    logging.info("**** Beginning The VPN Connection ****")
+    logging.info("** Beginning The VPN Connection **")
 
     num_connection_attempts = 0
 
     # Build the FSM 
-    default_FSM_memory = {'reset_connection'    : False,
-                          'response_to_pexpect' : None}
+    default_FSM_memory = {'reset_connection'        : False,    # Used to close pexpect session and open a new one
+                          'connection_established'  : False,    # Used to change pexpect timeout if connection established to block indefinitely
+                          'response_to_pexpect'     : None}     # Used to send string response back to pexpect
+    
     fsm = FSM (initial_state = 'CONNECTING', memory = default_FSM_memory)
     # fsm.add_transition_any     (state= 'SETUP', action = CreatePexpectProc, next_state = 'CONNECTING')
-    fsm.add_transition         (input_symbol=PASSWORD_REQUEST_SYMBOL,   state='CONNECTING', action=EnterPassword,        next_state='CONNECTING')
-    fsm.add_transition         (input_symbol=TWO_FACTOR_REQUEST_SYMBOL, state='CONNECTING', action=KillPexpectProc, next_state='SETUP')
+    fsm.add_transition         (input_symbol=PASSWORD_REQUEST_SYMBOL,      state='CONNECTING', action=EnterPassword,   next_state='CONNECTING')
+    fsm.add_transition         (input_symbol=TWO_FACTOR_REQUEST_SYMBOL,    state='CONNECTING', action=KillPexpectProc, next_state='CONNECTING')
+    fsm.add_transition         (input_symbol=SUCCESSFUL_CONNECTION_SYMBOL, state='CONNECTING', action=ConnEstablished, next_state='CONN_ESTABLISHED')
     fsm.set_default_transition (action=ErrorFSM, next_state='EXIT')
 
     # Configure pexpect object
@@ -66,7 +73,7 @@ def main() :
         # Create pexpect if first time or fsm.memory 
         if fsm.memory['reset_connection'] == True :
             num_connection_attempts += 1
-            logging.info(f"** Main Loop ** : Restarting pexpect. NOF attempts : {num_connection_attempts}. Exit status: {pexpect_child.exitstatus}, Signal status: {pexpect_child.signalstatus}")
+            logging.info(f"** Main Loop ** : Restarting pexpect. NOF attempts : {num_connection_attempts}")
             fsm.memory['reset_connection'] = False
             pexpect_child.close()
             pexpect_child = pexpect.spawn(CMD)
@@ -76,8 +83,12 @@ def main() :
         response = fsm.memory['response_to_pexpect']
         if response != None :
             pexpect_child.sendline(response)
+            fsm.memory['response_to_pexpect'] = None
 
-        i = pexpect_child.expect (EXPECTED_SYMBOLS, timeout = EXPECT_TIMEOUT)
+        pexpect_timeout = EXPECT_TIMEOUT
+        if fsm.memory['connection_established'] == True :
+            pexpect_timeout = None
+        i = pexpect_child.expect (EXPECTED_SYMBOLS, timeout = pexpect_timeout)
         fsm.process(EXPECTED_SYMBOLS[i])      
 
 if __name__ == "__main__" :
